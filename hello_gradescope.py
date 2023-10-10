4c04d949ccd9b7d14762cc0f6f78d7c7bade1395
aryap100 <patilar@oregonstate.edu>
2023-10-10 00:02:33 -0700

    Testing if this works

--- hello_gradescope.py
+++ hello_gradescope.py
@@ -1,7 +1,7 @@
 # Name: Arya Patil
 # OSU Email: patilar@oregonstate.edu
 # Course: CS261 - Data Structures
-# Assignment:0 intro
+# Assignment:0
 # Due Date:10/9/23
 # Description: Test to see whether python related code can be submitted into gradescope
 def gradescope_test():
